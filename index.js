9d0ad7d8a8d86eee87306a8d09502342387db117
sprusr <scott@spru.sr>
2018-03-10 15:12:09 +0000

    Implement date parsing for issue titles
    Fetches regex and date format from .github config.

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+var moment = require('moment')
+
 module.exports = (robot) => {
-  // Your code here
   robot.log('Yay, the app was loaded!')
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  const defaultConfig = {
+    regex: '(\d{4})-(\d{2})-(\d{2})$',
+    format: 'YYYY-MM-DD'
+  }
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  robot.on('issues.opened', async context => {
+    const config = await context.config('calendar.yml', defaultConfig)
+
+    const issueId = context.payload.issue.id
+    const issueTitle = context.payload.issue.title
+
+    const dateMatches = issueTitle.match(config.regex)
+    if (dateMatches.length == 0) return false
+
+    const issueDate = moment(dateMatches[0], config.format)
+
+    robot.log(issueDate)
+
+    // do gcal stuff
+  })
+
+  robot.on('issues.edited', context => {
+    const issueId = context.payload.issue.id
+    const issueTitle = context.payload.issue.title
+  })
 }
